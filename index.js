761fd066de0c0dad17a4e3060d9df0f57b6f6bca
ligjeronko <ligjeronko1@gmail.com>
2025-06-06 22:57:46 +0200

    index changest test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Jenkins CI/CD!');
+    res.send('HELLO, Jenkins CI/CD!');
 });
 
 const PORT = process.env.PORT || 3000;
