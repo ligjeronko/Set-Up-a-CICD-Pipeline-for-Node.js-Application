4ee2926a2879cbd01e9223b68ee088ee997d4e08
ligjeronko <ligjeronko1@gmail.com>
2025-06-07 00:47:53 +0200

    HELLOOO update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('HELLO, Jenkins CI/CD!');
+    res.send('HELLOOO, Jenkins CI/CD!');
 });
 
 const PORT = process.env.PORT || 3000;
